9881e7e1c789770ad665a886f87d004f4448e9d3
Leandro da silva <leandrosilva.hair@gmail.com>
2021-08-17 09:08:12 -0300

    upload file styleglobal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,17 +1,37 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-   *{
+
+  * {
       margin: 0;
       padding: 0;
+      outline: 0;
       box-sizing: border-box;
-   }
+      
+  }
 
-   html{
-      font-size: 62.5%;
-   }
+  body { 
+    background: linear-gradient(359deg, #171d25, #000000);
+    background-size: 400% 400%;
+    -webkit-font-smoothing: antialiased;
+  }
 
-   li{
+  body, input, button {
+      font: 16px Roboto, sans-serif;
+  }
+
+  #root {
+      max-width: 960px;
+      margin: 0 auto;
+      padding: 40px 20px;
+      
+  }
+
+  button {
+      cursor: pointer;
+  }
+
+  li{
       list-style: none;
-   } 
+  } 
 `;
